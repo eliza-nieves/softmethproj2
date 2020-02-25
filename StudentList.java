334cc3e4336290900ccb293fd58c24fea3e03678
Eliza Nieves <eliza.nieves@rutgers.edu>
2020-02-25 14:15:07 -0500

    Added in skeleton classes

---
+++ StudentList.java
@@ -0,0 +1,18 @@
+/**
+ @author Eliza Nieves
+ */
+public class StudentList
+{
+
+   public void add(){
+
+   }
+
+   public boolean remove(){
+
+   }
+
+   public void print(){
+
+   }
+}
---
+++ TuitionManager.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+/**
+ @author Eliza Nieves
+ */
+public class TuitionManager
+{
+   Scanner stdin = new Scanner(System.in);
+
+   public void run(){
+     
+   }
+
+   private void add(){
+    
+   }
+
+   private void remove(){
+
+   }
+
+   private void print(){
+
+   }
+
+} //ProjectManager
