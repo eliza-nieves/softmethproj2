4ebe9495b4ff04265a73ac7e7bf5450cc4caebf6
Mohammad <mohammad@Viveks-MBP.hsd1.nj.comcast.net>
2020-02-29 13:56:23 -0500

    Completed Instate and Student

--- Student.java
+++ Student.java
@@ -1,3 +1,8 @@
+/**
+ @author Eliza Nieves
+ @author Raheel Ozair
+ */
+
 public abstract class Student implements Comparable {
   private String fname;
   private String lname;
@@ -8,12 +13,32 @@ public abstract class Student implements Comparable {
     this.credit = credit;
   }
   //constructor
-  //must implement compareTo method because Student class implements the Comparable Interface
+  //must implement compareTo method because Student class implements the
+  //Comparable Interface
   //return 0 if fname and lname of the two students are equal,
-  //return -1 if this fname and lname is < obj’s, return 1 if this fname and lname is > obj’s
-  //Hint: use the .equals and compareToIgnoreCase methods of the String class to compare fname
+  //return -1 if this fname and lname is < obj’s, return 1 if this fname and
+  //lname is > obj’s
+  //Hint: use the .equals and compareToIgnoreCase methods of the String class
+  //to compare fname
   //and lname; names are not case-sensitive
-  public int compareTo(Object obj)
+  public int compareTo(Object obj){
+    if(!(obj instanceof Student)){
+      System.out.println("This is not a student.");
+      return -2;
+    }
+    Student compStudent = (Student) obj;
+    int longerName = (this.fname.compareToIgnoreCase(compStudent.fname) +
+      this.lname.compareToIgnoreCase(compStudent.lname)); //this will be negative if obj is longer
+
+    if(longerName < 0){
+      return -1;
+    }else if(longerName > 0){
+      return 1;
+    }else{
+      return 0;
+    }
+
+  }
   //return a string with fname, lname and credit hours; subclasses will
   //be using this method.
   public String toString() {
@@ -21,11 +46,6 @@ public abstract class Student implements Comparable {
     return retStr;
   }
   //compute the tuition due; concrete implementation is required in the subclasses.
-  public abstract int tuitionDue() {...}
+  public abstract int tuitionDue();
 
-}
-
-public static void main(String [] args){
-  Student s1 = new Student("Raheel","Ozair",1000);
-  System.out.println(s1.toString());
 }
